e07b14f3be7aaf11f100a4635236e2c0f41692c1
Jeff Ripke <jripke74@gmail.com>
2025-03-08 08:55:55 -0800

    add punctuationCount  and console.log()

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function getConsonantCount(sentence) {
 
 const consonantCount = getConsonantCount("Coding is fun");
 console.log(`Consonant Count: ${consonantCount}`);
+
+function getPunctuationCount(sentence) {
+  const punctuations = ".,!?;:-()[]{}\"'–";
+  let count = 0;
+
+  for (const char of sentence) {
+    if (punctuations.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Punctuation Count: ${punctuationCount}`);
