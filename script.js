71054fc064b50a5cffecce9730e26960967013c6
Jeff Ripke <jripke74@gmail.com>
2025-03-08 08:23:16 -0800

    add getConsonantCount()

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function getVowelCount(sentence) {
 
 const vowelCount = getVowelCount("Apples are tasty fruits");
 console.log(`Vowel Count: ${vowelCount}`);
+
+function getConsonantCount(sentence) {
+  const consonants = "bcdfghjklmnpqrstvwxyz";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (consonants.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
